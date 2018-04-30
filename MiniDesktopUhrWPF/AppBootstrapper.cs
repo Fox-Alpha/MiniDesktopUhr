f74a5b361ff1162440a7df0e121b1db1d422767a
Fox-Alpha <fox-alpha@tarnhoerner.de>
2018-04-30 19:53:10 +0200

    Upd: Default Bootstraper für Caliburn Micro

--- MiniDesktopUhrWPF/AppBootstrapper.cs
+++ MiniDesktopUhrWPF/AppBootstrapper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using Caliburn.Micro;
 
 namespace MiniDesktopUhrWPF
@@ -17,7 +18,26 @@ namespace MiniDesktopUhrWPF
 
 		protected override void OnStartup (object sender, StartupEventArgs e)
 		{
-			DisplayRootViewFor<AppViewModel> ();
+			Dictionary<string, object> setting = new Dictionary<string, object> ();
+			setting.Add ("WindowStyle", WindowStyle.None);
+			setting.Add ("ResizeMode", ResizeMode.NoResize);
+
+			DisplayRootViewFor<AppViewModel> (setting);
+		}
+
+		protected override void OnExit (object sender, EventArgs e)
+		{
+			base.OnExit (sender, e);
+		}
+
+		protected override void OnUnhandledException (object sender, DispatcherUnhandledExceptionEventArgs e)
+		{
+			base.OnUnhandledException (sender, e);
+		}
+
+		protected override void Configure ()
+		{
+			base.Configure ();
 		}
 	}
 }
