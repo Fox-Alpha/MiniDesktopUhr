d7741181bbb4363a3ed66587540aa22bad202b5f
Fox-Alpha <fox-alpha@tarnhoerner.de>
2019-09-10 01:09:17 +0200

    Add: Helper Klasse für den Ersatz der WinForm KLasse Screens

---
+++ MiniDesktopUhrWPF/Models/MonitorModel.cs
@@ -0,0 +1,42 @@
+﻿using MiniDesktopUhrWPF.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MiniDesktopUhrWPF.Models
+{
+    public class MonitorModel
+    {
+        private Screen[] _screens;
+        public Screen[] Screens { get => _screens; set => _screens = value; }
+
+        public MonitorModel()
+        {
+            Screens = Screen.AllScreens.ToArray();
+        }        
+
+        public int GetPrimaryScreenIndex()
+        {
+            int activeDisplay = -1;
+
+            foreach (var scr in Screen.AllScreens)
+            {
+                if (scr.Primary)
+                {
+                    activeDisplay = Screen.AllScreens.ToList().IndexOf(scr);
+                    break;
+                }
+            }
+
+            return activeDisplay;
+        }
+
+        public Screen GetPrimaryScreen()
+        {
+            return Screens[GetPrimaryScreenIndex()];
+        }
+    }
+}
