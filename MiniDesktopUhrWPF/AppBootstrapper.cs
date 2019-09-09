90f741bce35a2836cb8d8cc49908f0779d892aab
Fox-Alpha <fox-alpha@tarnhoerner.de>
2019-09-09 23:33:56 +0200

    Upd: CaliburnMicro Aktualisierung der Methoden

--- MiniDesktopUhrWPF/AppBootstrapper.cs
+++ MiniDesktopUhrWPF/AppBootstrapper.cs
@@ -6,23 +6,42 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 using Caliburn.Micro;
+using MiniDesktopUhrWPF.ViewModels;
 
 namespace MiniDesktopUhrWPF
 {
-	public class AppBootstrapper : Caliburn.Micro.BootstrapperBase
+	public class AppBootstrapper : BootstrapperBase
 	{
-		public AppBootstrapper()
+        private SimpleContainer _container = new SimpleContainer();
+
+        public AppBootstrapper()
 		{
 			Initialize ();
 		}
 
-		protected override void OnStartup (object sender, StartupEventArgs e)
-		{
-			Dictionary<string, object> setting = new Dictionary<string, object> ();
-			setting.Add ("WindowStyle", WindowStyle.None);
-			setting.Add ("ResizeMode", ResizeMode.NoResize);
+        protected override void Configure()
+        {
+            _container.Instance(_container);
 
-			DisplayRootViewFor<AppViewModel> (setting);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+            Dictionary<string, object> setting = new Dictionary<string, object>();
+            setting.Add("WindowStyle", WindowStyle.None);
+            setting.Add("ResizeMode", ResizeMode.NoResize);
+        }
+
+        protected override void OnStartup (object sender, StartupEventArgs e)
+		{
+			DisplayRootViewFor<ShellViewModel>();
 		}
 
 		protected override void OnExit (object sender, EventArgs e)
@@ -30,14 +49,24 @@ namespace MiniDesktopUhrWPF
 			base.OnExit (sender, e);
 		}
 
-		protected override void OnUnhandledException (object sender, DispatcherUnhandledExceptionEventArgs e)
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void OnUnhandledException (object sender, DispatcherUnhandledExceptionEventArgs e)
 		{
 			base.OnUnhandledException (sender, e);
-		}
-
-		protected override void Configure ()
-		{
-			base.Configure ();
 		}
 	}
 }
